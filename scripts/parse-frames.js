6b773c480b19758a51c8fa687a3d74db01a211cf
Brian Hann <brian.hann@gmail.com>
2018-06-13 14:32:17 -0500

    Use cluster module to process frames in parallel for parse-frames.js

--- scripts/parse-frames.js
+++ scripts/parse-frames.js
@@ -1,26 +1,134 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const cluster = require('cluster');
+const os = require('os');
 const path = require('path');
 const _ = require('lodash');
+const chalk = require('chalk');
 const Jimp = require('jimp');
+const meow = require('meow');
 const recursive = require('recursive-readdir');
 
-// const framePath = path.join(__dirname, '..', 'Facebook Devices');
-const framePath = path.join(__dirname, '../node_modules/deviceframe-frames');
+const cli = meow(`
+  Usage
+    # Run a test
+    $ node scripts/parse-frames.js
+    # Run and write out results to ../data/frames.json
+    $ node scripts/parse-frames.js -w
 
+  Options
+    --write, -w     Write out results to ../data/frames.json
+    --count, -c     Run on limited number of frames. Ex: '-c 5' will do 5 frames then stop
+    --parallel, -p  Run N multiple workers at once. Ex: '-p 4' if you have 4 cores
+`,
+{
+  flags: {
+    help: {
+      alias: 'h',
+    },
+    write: {
+      alias: 'w',
+      default: false,
+    },
+    count: {
+      type: 'number',
+      alias: 'c',
+      default: 0,
+    },
+    parallel: {
+      alias: 'p',
+      default: false,
+    },
+    output: {
+      type: 'string',
+      alias: 'o',
+      default: '.',
+    },
+    // debug: {
+    //   type: 'boolean',
+    //   alias: 'd',
+    //   default: false,
+    // }
+  },
+});
+
+const start = new Date().getTime();
+
+const framePath = path.join(__dirname, '../node_modules/deviceframe-frames');
 const px = require('../data/pixel-ratios.json');
 
-readFrames()
-.then(frames => {
-  const content = JSON.stringify(frames, null, 4);
-  fs.writeFileSync(path.join(__dirname, '../data/frames.json'), content);
-});
+// Only spin up things if you're the master
+if (cluster.isMaster) {
+  readFrames()
+    .then(frames => {
+      console.log(chalk.green(`Processed ${frames.length} frames with ${cli.flags.p} workers`));
+      const end = new Date().getTime();
+      const took = ((end - start) / 1000).toFixed(1);
+      console.log(chalk.magenta(`Took ${took}s`));
+
+      const content = JSON.stringify(frames, null, 4);
+
+      if (cli.flags.write) fs.writeFileSync(path.join(__dirname, '../data/frames.json'), content);
+    });
+} else {
+  // console.log(`Worker ${process.pid} is running`);
+  // Worker
+  process.on('message', message => {
+    // console.log(`Worker #${cluster.worker.id}`, message);
+
+    Promise.all(message.data.map(file => getFrameDetails(file)))
+      .then(result => {
+        process.send({ result });
+        process.exit(0);
+      });
+  });
+
+  // process.exit(0);
+}
 
 function readFrames() {
   return recursive(framePath)
-  .then(files => files.filter(path => /\.png$/.test(path)))
-  .then(files => files.sort())
-  .then(files => Promise.all(files.map(file => getFrameDetails(file))));
+    .then(files => files.filter(path => /\.png$/.test(path)))
+    .then(files => files.sort())
+    .then(files => {
+      if (cli.flags.count > 0) return files.slice(0, cli.flags.count);
+      return files;
+    })
+    .then(files => {
+      if (cli.flags.p) {
+        let workerCount = parseInt(cli.flags.p, 10);
+        if (isNaN(workerCount) || !workerCount) workerCount = os.cpus().length;
+
+        if (cluster.isMaster) {
+          const chunks = [];
+          const chunkSize = Math.ceil(files.length / workerCount);
+          for (let i = 0; i < workerCount; i++) {
+            cluster.fork();
+            chunks.push(files.slice(i * chunkSize, (i * chunkSize) + chunkSize));
+          }
+
+          let results = [];
+          for (const id in cluster.workers) { /* eslint guard-for-in: 0 */
+            cluster.workers[id].on('message', message => {
+              // console.log(`Master ${process.pid}`, message);
+              if (message.result) results = results.concat(message.result);
+            });
+
+            cluster.workers[id].send({ data: chunks.shift() });
+          }
+
+          return new Promise(resolve => {
+            cluster.on('exit', () => {
+              // console.log('workers', Object.keys(cluster.workers).length);
+
+              if (Object.keys(cluster.workers).length === 0) resolve(results);
+            });
+          });
+        }
+      } else {
+        return Promise.all(files.map(file => getFrameDetails(file)));
+      }
+    });
 }
 
 function getFrameDetails(framePath) {
@@ -30,24 +138,24 @@ function getFrameDetails(framePath) {
   const shadow = /shadow/i.test(relPath);
 
   return pathToFrame(framePath)
-  .then(frame => {
-    console.log([category, device, name, shadow ? 'Shadow' : 'No Shadow'].join(' | '));
+    .then(frame => {
+      console.log([category, device, name, shadow ? 'Shadow' : 'No Shadow'].join(' | '));
 
-    let pixelRatio = null;
-    const p = _.find(px, { name: device });
-    if (p) pixelRatio = p.pixelRatio;
+      let pixelRatio = null;
+      const p = _.find(px, { name: device });
+      if (p) pixelRatio = p.pixelRatio;
 
-    return {
-      relPath,
-      category,
-      device,
-      frame,
-      name,
-      pixelRatio,
-      shadow,
-      tags: name.split(/\s+/).map(tag => tag.toLowerCase()),
-    };
-  });
+      return {
+        relPath,
+        category,
+        device,
+        frame,
+        name,
+        pixelRatio,
+        shadow,
+        tags: name.split(/\s+/).map(tag => tag.toLowerCase()),
+      };
+    });
 }
 
 function pathToFrame(framePath) {
